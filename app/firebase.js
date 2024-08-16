98a5950e0a4babbb6ae2255ad48aefa2c1345792
rlal2022 <lrahul2010@gmail.com>
2024-08-16 04:10:45 -0700

    fix security issue

--- app/firebase.js
+++ app/firebase.js
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC0EY938m_9PE-yIe9RVou3L6dnn2WSm5A",
-  authDomain: "flashcards-saas-d446b.firebaseapp.com",
-  projectId: "flashcards-saas-d446b",
-  storageBucket: "flashcards-saas-d446b.appspot.com",
-  messagingSenderId: "904385397248",
-  appId: "1:904385397248:web:39942244b62d12b3d78ee5",
-  measurementId: "G-KBVRCW7P7P",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
